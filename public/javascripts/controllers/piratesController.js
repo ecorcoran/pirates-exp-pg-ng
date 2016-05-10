5a15a7ba7a95ccfa53fde6efb6cfb7636953b8ff
Emily Platzer <emilyplatzer@gmail.com>
2016-05-10 15:54:50 -0600

    Pirates are rendered in view

--- public/javascripts/controllers/piratesController.js
+++ public/javascripts/controllers/piratesController.js
@@ -2,6 +2,6 @@ var app = angular.module('pirates')
 
 app.controller('PiratesController', function ($scope, PiratesService) {
   PiratesService.all().then(function (res) {
-    console.log(res.data);
+    $scope.pirates = res.data;
   });
 })
