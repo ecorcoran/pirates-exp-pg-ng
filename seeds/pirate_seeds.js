a379392952f7deeee1dff2c61d52327d6c8ef52b
Emily Platzer <emilyplatzer@gmail.com>
2016-05-11 14:09:45 -0600

    Seed files - no id, in array

--- seeds/pirate_seeds.js
+++ seeds/pirate_seeds.js
@@ -5,31 +5,26 @@ exports.seed = function(knex, Promise) {
 
     // Inserts seed entries
     knex('pirates').insert(
-      {
-        id: 1,
-        name: 'Anne Bonney',
-        poison: 'Rum',
-        accessory: 'hot temper',
-        image_url: 'http://2.bp.blogspot.com/-WSKMEi_MH5U/U-AdUnMNuvI/AAAAAAAABI8/fv0BXLICx8c/s1600/Anne_Bonny_color.jpg'
-      }
-    ),
-    knex('pirates').insert(
-      {
-        id: 2,
-        name: 'Awilda',
-        poison: 'brave princes',
-        accessory: 'badass lady friends',
-        image_url: 'https://upload.wikimedia.org/wikipedia/commons/e/ed/Awilda.jpg'
-      }
-    ),
-    knex('pirates').insert(
-      {
-        id: 3,
-        name: 'Ching Shih',
-        poison: 'Opium',
-        accessory: 'Governmenal pardon',
-        image_url: 'https://upload.wikimedia.org/wikipedia/commons/1/19/%E9%84%AD%E4%B8%80%E5%AB%82.jpg'
-      }
+      [
+        {
+          name: 'Anne Bonney',
+          poison: 'Rum',
+          accessory: 'hot temper',
+          image_url: 'http://2.bp.blogspot.com/-WSKMEi_MH5U/U-AdUnMNuvI/AAAAAAAABI8/fv0BXLICx8c/s1600/Anne_Bonny_color.jpg'
+        },
+        {
+          name: 'Awilda',
+          poison: 'brave princes',
+          accessory: 'badass lady friends',
+          image_url: 'https://upload.wikimedia.org/wikipedia/commons/e/ed/Awilda.jpg'
+        },
+        {
+          name: 'Ching Shih',
+          poison: 'Opium',
+          accessory: 'Governmenal pardon',
+          image_url: 'https://upload.wikimedia.org/wikipedia/commons/1/19/%E9%84%AD%E4%B8%80%E5%AB%82.jpg'
+        }
+      ]
     )
-  );
+  )
 };
